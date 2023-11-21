0cd832bde76336709a8e37bf2a53278c4371b19f
kawausokun-0529 <138553784+kawausokun-0529@users.noreply.github.com>
2023-11-21 17:34:40 +0900

    Update FighterBullet.java

--- FighterBullet.java
+++ FighterBullet.java
@@ -1,11 +1,13 @@
 import java.awt.*;
 
+//発射する弾のクラス
+
 class FighterBullet extends MovingObject{
     //constructor
     FighterBullet(){
-        w = h = 3;
+        w = h = 3;//弾の半径
         dx = 0; dy = -6;
-        hp = 0;
+        hp = 0;//初期状態は非アクティブ
     }
     
     //method
@@ -13,17 +15,17 @@ class FighterBullet extends MovingObject{
         if(hp>0){
             buf.setColor(Color.yellow);
             buf.fillOval(x-w, y-h, 2*w, 2*h);
-            if(y>0 && y < apHeight && x>0 && x<apWidth){
-                y = y + dy;
+            if(y>0 && y < apHeight && x>0 && x<apWidth){//弾が画面内にあれば
+                y = y + dy;//座標の更新
             }
             else{
-                hp = 0;
+                hp = 0;//画面買いに出たらhpを0にする
             }
         }
     }
-    void revive(int x, int y){
+    void revive(int x, int y){//x,yはFighterの位置
         this.x = x;
         this.y = y;
-        hp = 1;
+        hp = 1;//発射したらアクティブに
     }
 }
