29f4d24f760dbaf2ffc829ccbb9eaafe210ccbcd
kawausokun-0529 <138553784+kawausokun-0529@users.noreply.github.com>
2023-11-21 17:31:18 +0900

    Update EnemyA.java

--- EnemyA.java
+++ EnemyA.java
@@ -1,31 +1,31 @@
 import java.awt.*;
 
 class EnemyA extends MovingObject{
-    Image img = getToolkit().getImage("enemyA.png");
+    Image img = getToolkit().getImage("enemyA.png");//画像を読み込んでImageクラスのインスタンスに取り込む
 
     //constructor
     EnemyA(int apWidth, int apHeight){
-        super(apWidth, apHeight);
+        super(apWidth, apHeight);//親クラスのコンストラクタの呼び出し
         w = 12;
         h = 12;
-        hp = 0;
-        point = 10;
+        hp = 0;//初期状態
+        point = 10;//倒したときに入る得点
     }
 
     //method
-    void move(Graphics buf, int apWidth, int apHeight){
-        buf.setColor(Color.red);
-        if(hp>0){
+    void move(Graphics buf, int apWidth, int apHeight){//敵画像を使わないときのメソッド
+        buf.setColor(Color.red);//色を赤に設定
+        if(hp>0){//生きていれば
             //buf.setColor(Color.black);
-            buf.drawOval(x-w, y-h, 2*w, 2*h);
-            x = x + dx;
-            y = y + dy;
+            buf.drawOval(x-w, y-h, 2*w, 2*h);//〇を描く
+            x = x + dx;//x座標の更新
+            y = y + dy;//y座標の更新
             if(y>apHeight+h){
                 hp = 0;
             }
         }
     }
-    void move(Graphics gc, int apHeight){
+    void move(Graphics gc, int apHeight){//敵画像を表示するメソッド
         if(hp>0){
             gc.drawImage(img, x-w, y-h, this);
             x = x + dx;
@@ -35,7 +35,7 @@ class EnemyA extends MovingObject{
             }
         }
     }
-    void revive(int apWidth, int apHeight){
+    void revive(int apWidth, int apHeight){//敵を新たに生成する
         x = (int)(Math.random()*(apWidth-2*w)+w);
         y = -h;
         dy = 1;
