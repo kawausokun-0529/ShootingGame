57f09a2bd88c330096f8a2da10430bbc37536f53
kawausokun-0529 <138553784+kawausokun-0529@users.noreply.github.com>
2023-11-21 17:39:44 +0900

    Update Fighter.java

--- Fighter.java
+++ Fighter.java
@@ -1,19 +1,19 @@
 import java.awt.*;
 
 public class Fighter extends MovingObject{
-    boolean lflag;
-    boolean rflag;
-    boolean uflag;
-    boolean dflag;
-    boolean sflag;
-    int delaytime;
+    boolean lflag;//←が押されているか
+    boolean rflag;//→が押されているか
+    boolean uflag;//↑が押されているか
+    boolean dflag;//↓が押されているか
+    boolean sflag;//スペースキーが押されているか
+    int delaytime;//次の弾発射までの待ち時間
 
-    Image img = getToolkit().getImage("rocket.png");
+    Image img = getToolkit().getImage("rocket.png");//自機の画像をインスタンス化
 
     //constractor
     Fighter(int apWidth, int apHeight){
-        x = (int)(apWidth/2);
-        y = (int)(apHeight*0.9);
+        x = (int)(apWidth/2);//画面の真ん中
+        y = (int)(apHeight*0.9);//画面の下の方
         dx = 5;
         dy = 5;
         w = 32;
@@ -31,24 +31,24 @@ public class Fighter extends MovingObject{
     void revive(int apWidth, int apHeight){
     }
     
-    void move(Graphics buf, int apWidth, int apHeight){
+    void move(Graphics buf, int apWidth, int apHeight){//画像を取り込まない場合
         buf.setColor(Color.green);
         buf.fillRect(x-w, y-h, 2*w, 2*h);
-        if(lflag && !rflag && x>w){
-            x = x - dx;
+        if(lflag && !rflag && x>w){//←ON、→OFF、左の壁に当たっていない場合
+            x = x - dx;//オブジェクトを左に動かす
         }
-        if(rflag && !lflag && x<apWidth - w){
-            x = x + dx;
+        if(rflag && !lflag && x<apWidth - w){//→ON、←OFF、右の壁に当たっていない場合
+            x = x + dx;//オブジェクトを右に動かす
         }
-        if(uflag && !dflag && y>h){
-            y = y - dy;
+        if(uflag && !dflag && y>h){//↑ON、↓OFF、上の壁に当たっていない場合
+            y = y - dy;//オブジェクトを上に動かす
         }
-        if(dflag && !uflag && y<apHeight - h){
-            y = y + dy;
+        if(dflag && !uflag && y<apHeight - h){//↓ON、↑OFF、下の壁に当たっていない場合
+            y = y + dy;//オブジェクトを下に動かす
         }
     }
 
-    void move(Graphics gc){
+    void move(Graphics gc){//画像を取り込む場合
         gc.drawImage(img, x-w, y-h, this);
 
         if(lflag && !rflag && x>w){
